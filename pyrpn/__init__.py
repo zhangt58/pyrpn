120b3b61fc50c9e77341840fa33eda554f084a2c
Tong Zhang <zhangt@frib.msu.edu>
2018-01-11 14:52:14 -0500

    Update import policy.

--- pyrpn/__init__.py
+++ pyrpn/__init__.py
@@ -1,5 +1,7 @@
-from . import rpn
+from .rpn import Rpn
+from .rpn import solve_rpn
 
 __author__ = "Tong Zhang <warriorlance@gmail.com>"
 __version__ = "1.0.4"
 
+__all__ = ["Rpn", "rpn", "solve_rpn"]
